21b725a28e4233616cdd315b0d926ee03f0dd888
qhfhdls12 <qhfhdls12@naver.com>
2023-02-14 17:50:07 +0900

    add : 투포인터

---
+++ 2. 구현 예제/투포인터/투포인터.java
@@ -0,0 +1,18 @@
+static void twoPointerSearch() {
+		int start = 0;
+		int end = n - 1;
+
+		while (start < end) {
+			int tmp = nums[start] + nums[end];
+			
+			if (tmp == x) {
+				cnt++;
+			}
+
+			if (tmp > x) {
+				end--;
+			} else {
+				start++;
+			}
+		}
+	}
