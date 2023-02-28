669939780303855feffffc91d1ef2d46d845a511
wjs5025 <wjs_5025@naver.com>
2023-02-28 09:10:30 +0900

    chore : 파일 정리

---
+++ 2. 구현 예제/08. 분할정복/거듭제곱.java
@@ -0,0 +1,20 @@
+// 일반 재귀 거듭제곱
+private static long exp1(long x, long n) {
+    callCnt1++;
+    if (n == 1)
+        return x;
+
+    return x * exp1(x, n - 1);
+}
+
+// 분할 정복 거듭제곱ㄴ
+private static long exp2(long x, long n) {
+    callCnt2++;
+
+    if (n == 1)
+        return x;
+
+    long y = exp2(x, n / 2);
+
+    return n % 2 == 0 ? y * y : y * y * x;
+}
